e07d5f7e878742954bee94fae483c436d6f517c9
Mazzoni <lucasvascaino27@gmail.com>
2024-03-29 17:30:41 -0300

    Desafio banco

---
+++ src/desafio_banco/Cliente.java
@@ -0,0 +1,15 @@
+package desafio_banco;
+
+public class Cliente {
+
+	public String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
